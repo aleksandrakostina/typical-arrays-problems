7c8c9eb83bc44979c4f1d441eb2f2e1f90df0cce
aleksandrakostina <60540168+aleksandrakostina@users.noreply.github.com>
2020-03-04 13:52:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array)
+    return array.reduce(function (acc, cur){ return Math.min(acc, cur)}, 0);
+  else
+    return 0;
 }
 
 exports.max = function max (array) {
+  if (array)
+  return array.reduce(function (acc, cur){ return Math.max(acc, cur)}, 0);
+else
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array)
+  return array.reduce(function (acc, cur, index){ acc += cur; if(index == array.length-1) return acc/array.length
+  else return acc}, 0);
+else
   return 0;
 }
